68e473200a3b2774003c4fb2083667784eb6087d
杨洋 <yy@yangyangdeMacBook-Air.local>
2025-12-26 17:36:58 +0800

    1

--- config/route-mapping.ts
+++ config/route-mapping.ts
@@ -96,7 +96,7 @@ export const routeAliases: RouteAlias[] = [
   // 对于 (protected) 路由组内的页面，路径不需要包含路由组名称
   // 因为路由组在 URL 中不显示，但 Next.js 会自动在路由组内查找
   { alias: '/cq', target: '/JP/page1', blockedPage: '/white/JP/w1', buttonLinks: { primary: 'https://federenne.com/next01' } },
-  { alias: '/n', target: '/white/JP/book', blockedPage: '/white/JP/book', buttonLinks: { primary: 'https://federenne.com/next01' } },
+  { alias: '/n', target: '/JP/page1', blockedPage: '/white/JP/book', buttonLinks: { primary: 'https://federenne.com/next01' } },
   { alias: '/', target: '/white/sites' },
 ]
 
